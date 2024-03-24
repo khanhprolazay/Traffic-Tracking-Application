6ded7a51870c32dabe82bba4dd7a3093e9a8f427
Le Dang Minh <minhledang.hcm@gmail.com>
2024-03-24 15:52:46 +0700

    refactor: add app context

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,16 +4,21 @@ import App from "./App.jsx";
 import "./index.css";
 import { store } from "./app/store.js";
 import { Provider } from "react-redux";
-// import { AntdProvider, GoogleMapProvider } from "./providers";
 import AntdProvider from "./providers/antd.provider";
 import GoogleMapProvider from "./providers/google-map.provider";
 
+function AppContext({ chilren }) {
+	return (
+		<Provider store={store}>
+			<GoogleMapProvider>
+				<AntdProvider>{chilren}</AntdProvider>
+			</GoogleMapProvider>
+		</Provider>
+	);
+}
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-	<Provider store={store}>
-		<GoogleMapProvider>
-			<AntdProvider>
-				<App />
-			</AntdProvider>
-		</GoogleMapProvider>
-	</Provider>
+	<AppContext>
+		<App />
+	</AppContext>
 );
